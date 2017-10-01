f444f9d8ba5b4fc36f5513a1a7eb11a6830af5c4
LukeHoel <hoel.luke@gmail.com>
2017-10-01 21:14:08 +0300

    added side navbar

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,8 +1,13 @@
 function mainController () {
 
     var vm = this;
+    
+    // to access functions from html, it seems as though you need to declare like this
+    // and call with brackets, even though the variable vm.test has none
+    vm.test = function test(){
+        return "test";
+    }
 
-    vm.isNavbarOut=true;
 }
     
     angular
