855f052095b2c60ea96dea767b28d27b9b65aa6f
LukeHoel <hoel.luke@gmail.com>
2017-10-01 21:55:00 +0300

    updated colors

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,12 +1,17 @@
 function mainController () {
 
     var vm = this;
-    
     // to access functions from html, it seems as though you need to declare like this
     // and call with brackets, even though the variable vm.test has none
     vm.test = function test(){
         return "test";
     }
+
+    vm.links = [
+        {name : "Home", route: "/"},
+        {name : "Bio", route: "/bio"}
+    ]
+
 
 }
     
