35c5e4421df90799241d68119bb848d65ad2a5b7
Luke Hoel <hoel.luke@gmail.com>
2020-01-15 22:59:21 -0500

    added build to docs folder

---
+++ src/app/components/language/language.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-language',
+  templateUrl: './language.component.html',
+  styleUrls: ['./language.component.scss']
+})
+export class LanguageComponent {
+  @Input() language = "";
+}
